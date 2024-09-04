062c23950fb6f38e288f22ac8dcdec85c090e215
vanesamrd <paneja435@gmail.com>
2024-09-03 20:40:38 -0700

    feat: biodata vanesa

---
+++ biodataVanesa.java
@@ -0,0 +1,15 @@
+public class biodataVanesa {
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------------------------");
+        System.out.println("-- Data Biodata Vanesa --");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Nama\t\t: Vanesa Mardiana Putri");
+        System.out.println("NIM\t\t: 244107020129 ");
+        System.out.println("Kelas\t\t: 1B");
+        System.out.println("No Absen\t: 24");
+        System.out.println("Prodi\t\t: D-IV Teknik Informatika");
+        System.out.println("Alamat\t\t: ds. Kandangan RT02/03 Kec. Srengat Kab. Blitar");
+        System.out.println("No HP\t\t: 085785476987");
+        System.out.println("Hobi\t\t: Mendengar musik");
+    }
+}
